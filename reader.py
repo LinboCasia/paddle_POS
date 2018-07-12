68239404a44c8eb8eb7a929ee98d8560c1c29d82
LinboCasia <naibo987@163.com>
2018-07-12 10:46:38 +0800

    1.28

--- reader.py
+++ reader.py
@@ -5,7 +5,9 @@
 # Version 1.0
 # filename: reader.py
 import os
-def train_reader(data_dir, word_dict, label_dict, window_size=5):    """
+
+def train_reader(data_dir, word_dict, label_dict, window_size=5):
+    """
     Reader interface for training data
 
     :param data_dir: data directory
@@ -22,16 +24,21 @@ import os
         UNK_WID = word_dict["<UNK>"]
         UNK_LID = label_dict["<UNK>"]
         word_col, lbl_col = 0, 1
-        interest_word_window = int(window_size / 2)		
+        interest_word_window = int(window_size / 2)
+		
         for file_name in os.listdir(data_dir):
             with open(os.path.join(data_dir, file_name), "r") as f:
                 for line in f:
                     line_split = line.encode("utf-8").strip().split()
-                    					##sentence with a special "PADDING" word
+                    
+					##sentence with a special "PADDING" word
                     #      replicated window_size/2 times at the begining and end
                     # "PADDING" at the begining
-                    word_ids, label_ids = [UNK_WID] * interest_word_window, [                        UNK_LID                    ] * interest_word_window
-                    					for item in line_split:
+                    word_ids, label_ids = [UNK_WID] * interest_word_window, [
+                        UNK_LID
+                    ] * interest_word_window
+                    
+					for item in line_split:
                         try:
                             items = item.split("/")
                             w = word_dict.get(items[word_col].strip(), UNK_WID)
@@ -40,13 +47,20 @@ import os
                             label_ids.append(l)
                         except:
                             continue
-                    					#"PADDING" at the end
+                    
+					#"PADDING" at the end
                     word_ids += [UNK_WID] * interest_word_window
                     label_ids += [UNK_LID] * interest_word_window
-                    					if len(word_ids) < interest_word_window:
+                    
+					if len(word_ids) < interest_word_window:
                         continue
-                    					if len(word_ids) < window_size:
-                        yield word_ids + [UNK_WID] * (                            window_size - len(word_ids)                        ), label_ids[interest_word_window]
-                    					for i in range(len(word_ids) - window_size):
-                        yield word_ids[i:i + window_size], label_ids[                            i + interest_word_window]
-    return reader
+                    
+					if len(word_ids) < window_size:
+                        yield word_ids + [UNK_WID] * (
+                            window_size - len(word_ids)
+                        ), label_ids[interest_word_window]
+                    
+					for i in range(len(word_ids) - window_size):
+                        yield word_ids[i:i + window_size], label_ids[
+                            i + interest_word_window]
+    return reader
